1dd657cc69b2aab041de80364a8ad24c0ba722b8
lokesh mvs <lokeshmvs21@gmail.com>
2025-04-14 12:13:12 +0530

    docs: Added cloud related files

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,13 +2,21 @@
 import { Accordion, Accordions } from 'fumadocs-ui/components/accordion'
 import { File, Files, Folder } from 'fumadocs-ui/components/files'
 import { Step, Steps } from 'fumadocs-ui/components/steps'
+import { Card, Cards } from 'fumadocs-ui/components/card'
 import defaultComponents from 'fumadocs-ui/mdx'
 import type { MDXComponents } from 'mdx/types'
+
+import AWS from '@/app/(docs)/components/ui/SVG/AWS'
+import Azure from '@/app/(docs)/components/ui/SVG/Azure'
+import DigitalOcean from '@/app/(docs)/components/ui/SVG/DigitalOcean'
+import GCP from '@/app/(docs)/components/ui/SVG/GoogleCloudPlatform'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
     ...components,
+    Card,
+    Cards,
     File,
     Folder,
     Files,
@@ -16,5 +24,9 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     Steps,
     Accordion,
     Accordions,
+    AWS,
+    Azure,
+    DigitalOcean,
+    GCP,
   }
 }
