ab20634e6852175795023ba045be339be8991c2e
charanm927 <charanm927@gmail.com>
2025-04-14 17:19:35 +0530

    chore: update files

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 import { withPayload } from '@payloadcms/next/withPayload'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  editor: lexicalEditor(),
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
