792b69542eb307b924788a2c9c00d46a21303c7d
charanm927 <charanm927@gmail.com>
2025-04-14 17:14:59 +0530

    chore: created blogs

---
+++ src/collections/Blogs.ts
@@ -0,0 +1,74 @@
+import {
+  FixedToolbarFeature,
+  HeadingFeature,
+  HorizontalRuleFeature,
+  InlineToolbarFeature,
+  lexicalEditor,
+} from '@payloadcms/richtext-lexical'
+import { slugField } from '@/fields/slug'
+import type { CollectionConfig } from 'payload'
+
+export const Blogs: CollectionConfig = {
+  slug: 'blogs',
+  admin: {
+    useAsTitle: 'title',
+  },
+  fields: [
+    {
+      name: 'tag',
+      type: 'relationship',
+      relationTo: 'tags',
+      hasMany: true,
+    },
+    {
+      name: 'author',
+      type: 'relationship',
+      relationTo: 'users',
+      hasMany: false,
+      filterOptions: ({ relationTo }) => {
+        if (relationTo === 'users') {
+          return {
+            role: { equals: 'author' },
+          }
+        }
+
+        // Return true if the relationTo doesn't match, to show all options
+        return true
+      },
+    },
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'description',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'image',
+      type: 'upload',
+      relationTo: 'media',
+      required: true,
+    },
+    {
+      name: 'content',
+      type: 'richText',
+      editor: lexicalEditor({
+        features: ({ rootFeatures }) => {
+          return [
+            ...rootFeatures,
+            HeadingFeature({ enabledHeadingSizes: ['h1', 'h2', 'h3', 'h4'] }),
+            FixedToolbarFeature(),
+            InlineToolbarFeature(),
+            HorizontalRuleFeature(),
+          ]
+        },
+      }),
+      label: false,
+      required: true,
+    },
+    ...slugField(),
+  ],
+}
